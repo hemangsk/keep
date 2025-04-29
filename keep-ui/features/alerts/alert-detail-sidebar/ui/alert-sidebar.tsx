1eb1d469d4accc0867f22d6efbe48287dc1a07da
Vladimir Filonov <vladimir@filonov.dev>
2025-04-29 14:50:37 +0400

    Add support for displaying incident details in alerts
    Introduced `incident_dto` to enhance alerts with detailed incident data. Updated UI components to render incident links and names, leveraging `IncidentDto` structures. Adjusted backend logic to include and process `incident_dto` for enriched incident representation.

--- keep-ui/features/alerts/alert-detail-sidebar/ui/alert-sidebar.tsx
+++ keep-ui/features/alerts/alert-detail-sidebar/ui/alert-sidebar.tsx
@@ -16,6 +16,8 @@ import { useProviders } from "@/utils/hooks/useProviders";
 import { AlertMenu } from "@/features/alerts/alert-menu";
 import { useConfig } from "@/utils/hooks/useConfig";
 import { FormattedContent } from "@/shared/ui/FormattedContent/FormattedContent";
+import {Incident} from "@/features/incidents/incident-list/ui/incidents-report/models";
+import {IncidentDto} from "@/entities/incidents/model";
 
 type AlertSidebarProps = {
   isOpen: boolean;
@@ -168,6 +170,22 @@ export const AlertSidebar = ({
                     {alert.fingerprint}
                   </p>
                 </div>
+                {alert.incident_dto && <div>
+                  <FieldHeader>Incidents</FieldHeader>
+                  {alert.incident_dto.map((incident: IncidentDto) => {
+                    const title =
+                      incident.user_generated_name || incident.ai_generated_name;
+                    return (
+                      <Link
+                        key={incident.id}
+                        href={`/incidents/${incident.id}`}
+                        title={title}
+                      >
+                        {title}
+                      </Link>
+                    );
+                  })}
+                </div>}
                 <AlertTimeline
                   key={auditData ? auditData.length : 1}
                   alert={alert}
